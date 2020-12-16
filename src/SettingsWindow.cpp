90ac4857fd144599e2472b2a3c0da7c65a764cf8
HenriCSHC <henri.meissner@gmx.de>
2020-12-16 09:01:45 +0100

    Tried different widgets

--- src/SettingsWindow.cpp
+++ src/SettingsWindow.cpp
@@ -1,6 +1,41 @@
 #include "SettingsWindow.hpp"
 #include <iostream>
 
+Slider::Slider()
+{
+    set_range(0.0, 1.0);
+    set_digits(3);
+    set_value_pos(Gtk::POS_TOP);
+    set_draw_value();
+    set_inverted();
+    set_value(0.5);
+}
+
+void Slider::on_value_changed()
+{
+    std::cout << "Slider changed: " << get_value() << std::endl;
+}
+
+bool ScopeArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+    Gtk::Allocation allocation = get_allocation();
+    const int width = allocation.get_width();
+    const int height = allocation.get_height();
+
+    int xc = width / 2;
+    int yc = height / 2;
+
+    cr->set_line_width(2.0);
+    cr->set_source_rgb(0.0, 0.5, 1.0);
+    cr->move_to(xc, 0);
+    cr->line_to(xc, height);
+    cr->move_to(0, yc);
+    cr->line_to(width, yc);
+    cr->stroke();
+
+    return true;
+}
+
 SettingsWindow::SettingsWindow() 
 {
 }
@@ -13,6 +48,7 @@ SettingsWindow::~SettingsWindow()
 void SettingsWindow::Fill(std::vector<std::string> &inputPorts, std::vector<std::string> &outputPorts)
 {
     set_title("[MckSynth] Midi Settings");
+    set_default_size(300, 400);
 
     for (unsigned i = 0; i < inputPorts.size(); i++)
     {
@@ -25,11 +61,16 @@ void SettingsWindow::Fill(std::vector<std::string> &inputPorts, std::vector<std:
     }
     m_outputCombo.signal_changed().connect(sigc::mem_fun(*this, &SettingsWindow::on_output_changed));
 
-    m_box.set_orientation(Gtk::ORIENTATION_VERTICAL);
-    m_box.pack_start(m_inputCombo);
-    m_box.pack_start(m_outputCombo);
+    m_vertBox.set_spacing(5);
+    m_vertBox.set_orientation(Gtk::ORIENTATION_VERTICAL);
+    m_vertBox.pack_start(m_inputCombo, Gtk::PACK_SHRINK);
+    m_vertBox.pack_start(m_outputCombo, Gtk::PACK_SHRINK);
+    m_vertBox.pack_start(m_scope, Gtk::PACK_EXPAND_WIDGET);
 
-    add(m_box);
+    m_horBox.add(m_slider);
+    m_horBox.add(m_vertBox);
+
+    add(m_horBox);
     show_all_children();
 }
 
--- src/SettingsWindow.hpp
+++ src/SettingsWindow.hpp
@@ -4,6 +4,28 @@
 #include <gtkmm/window.h>
 #include <gtkmm/comboboxtext.h>
 #include <gtkmm/box.h>
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/hvscale.h>
+
+class Slider : public Gtk::VScale
+{
+    public:
+        Slider();
+        virtual ~Slider() {};
+
+    protected:
+        void on_value_changed() override;
+};
+
+class ScopeArea : public Gtk::DrawingArea
+{
+    public:
+        ScopeArea() {};
+        virtual ~ScopeArea() {};
+
+    protected:
+        bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+};
 
 class SettingsWindow : public Gtk::Window
 {
@@ -19,7 +41,10 @@ class SettingsWindow : public Gtk::Window
 
         Gtk::ComboBoxText m_inputCombo;
         Gtk::ComboBoxText m_outputCombo;
-        Gtk::Box m_box;
+        Gtk::Box m_horBox;
+        Gtk::Box m_vertBox;
+        ScopeArea m_scope;
+        Slider m_slider;
 
         std::vector<std::string> m_inputPorts;
         std::vector<std::string> m_outputPorts;
